11191753ad3aedeaea61840854ec0ce0f076dce5
yu-jeffy <105130385+yu-jeffy@users.noreply.github.com>
2022-10-31 10:27:47 -0700

    v1

---
+++ iwashere/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'iwashere-42cfa',
+    appId: '1:681582116256:web:e35d356356bf7f0541b8c5',
+    databaseURL: 'https://iwashere-42cfa-default-rtdb.firebaseio.com',
+    storageBucket: 'iwashere-42cfa.appspot.com',
+    apiKey: 'AIzaSyBGwqAfCFRzgXwn9W787km09Sme9jplCWI',
+    authDomain: 'iwashere-42cfa.firebaseapp.com',
+    messagingSenderId: '681582116256',
+    measurementId: 'G-C35FF6WPQ0',
+  },
+  production: true
+};
---
+++ iwashere/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'iwashere-42cfa',
+    appId: '1:681582116256:web:e35d356356bf7f0541b8c5',
+    databaseURL: 'https://iwashere-42cfa-default-rtdb.firebaseio.com',
+    storageBucket: 'iwashere-42cfa.appspot.com',
+    apiKey: 'AIzaSyBGwqAfCFRzgXwn9W787km09Sme9jplCWI',
+    authDomain: 'iwashere-42cfa.firebaseapp.com',
+    messagingSenderId: '681582116256',
+    measurementId: 'G-C35FF6WPQ0',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
